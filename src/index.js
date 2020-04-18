37a3ad61ccc8cb48665366abde64c85e4b7a9037
felipe <fajreyy.bh@gmail.com>
2020-04-18 14:18:16 -0300

    android fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import Routes from '~/routes';
+import CodePush from 'react-native-code-push';
 
 const App = () => <Routes />;
 
-export default App;
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
